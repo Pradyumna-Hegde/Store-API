5990f4a854329fb6d92829142b2af8cffe83af50
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2023-02-27 15:02:30 +0530

    added the SelectByField and Sorting codes for API

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,43 @@
 import Product from "../models/productsModel.js";
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: "dining table" });
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products route" });
+  const { featured, company, name, sort, fields } = req.query;
+  const queries = {};
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const result = Product.find(queries);
+
+  if (featured) {
+    queries.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queries.company = company;
+  }
+
+  if (name) {
+    queries.name = { $regex: name, $options: "i" };
+  }
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result.sort(sortList);
+  } else {
+    result.sort("createAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result.select(fieldsList);
+  }
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 export { getAllProductsStatic, getAllProducts };
--- populate.js
+++ populate.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import connect from "./db/connect.js";
-import Product from "./models/productModel.js";
+import Product from "./models/productsModel.js";
 import jsonProducts from "./products.json" assert { type: "json" };
 
 const uri = process.env.URI;
@@ -9,14 +9,7 @@ async function start() {
   try {
     await connect(uri);
     console.log(`Connected to Database`);
-    await Product.deleteMany();
-    console.log("Deleted the previous database records.");
-    await Product.create(jsonProducts);
-    console.log(
-      "Created all the records and wrote in to the collection: Products"
-    );
     console.log(`Operation Successfull...!`);
-    process.exit(0);
   } catch (error) {
     console.log(error);
     process.exit(1);
